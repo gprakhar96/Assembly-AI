2efaceb6ab74c25bfe699735f0f967f538853009
priyankmahour <mahourpriyank40@gmail.com>
2024-01-06 21:14:02 +0530

    Querry view-1

--- backend/assembly/upload/urls.py
+++ backend/assembly/upload/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('upload', views.upload_view),
+    path('querry', views.querry_view),
 ]
--- backend/assembly/upload/views.py
+++ backend/assembly/upload/views.py
@@ -1,7 +1,7 @@
 import os
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
@@ -25,3 +25,29 @@ def upload_view(request):
         return HttpResponse(json.dumps(resp),content_type="application/json")
     print("not okay")
     return HttpResponse('Invalid file type', status=400)
+
+
+
+@csrf_exempt 
+def querry_view(request):
+    if request.method == 'POST':
+        try:
+            # Expecting data is sent as JSON in the request body
+            data = json.loads(request.body.decode('utf-8'))
+
+            # Extract data from the request as needed
+            # For example, if the JSON data has a key 'querry', access it like:
+            my_data = data.get('querry')
+
+            # My processing logic here...
+
+            print(my_data)
+            response_data = {'message': my_data}
+            return JsonResponse(response_data, status=200)
+        except json.JSONDecodeError as e:
+            error_message = {'error': 'Invalid JSON format'}
+            return JsonResponse(error_message, status=400)
+
+    else:
+        # Return a method not allowed response for other HTTP methods
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
