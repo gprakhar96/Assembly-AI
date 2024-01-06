4dc8dbab8aa864e6ed14bcc2e9cfdf817aa2d7f8
Prakhar Gupta <guptaprakhar272@gmail.com>
2024-01-06 15:53:37 +0530

    Test

--- backend/assembly/assembly/settings.py
+++ backend/assembly/assembly/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -27,7 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.4.240:3000",
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -69,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'assembly.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -79,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -110,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- backend/assembly/upload/views.py
+++ backend/assembly/upload/views.py
@@ -1,7 +1,10 @@
+import os
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 from django.http import HttpResponse
-from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-
+from django.views.decorators.http import require_POST
+from docarray import DocumentArray
 
 # Create your views here.
 
@@ -11,6 +14,14 @@ from django.views.decorators.csrf import csrf_exempt
 def upload_view(request):
     manual_pdf = request.FILES.get('manual_pdf')
     if manual_pdf and manual_pdf.content_type == 'application/pdf':
-        # Process the PDF file
+        # Temporary store the PDF file
+
+        path = default_storage.save('temp.pdf', ContentFile(manual_pdf.read()))
+        tmp_file = os.path.join(default_storage.location, path)
+
+        print(tmp_file)
+        doc = DocumentArray.from_files(tmp_file)
+        print(type(doc))
+
         return HttpResponse('{} file received'.format(manual_pdf.name))
     return HttpResponse('Invalid file type', status=400)
