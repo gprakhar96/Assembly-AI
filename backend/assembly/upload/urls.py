90af0360975769d6405fb89c238f95a18573c646
Prakhar Gupta <guptaprakhar272@gmail.com>
2024-01-06 14:43:37 +0530

    Recieved PDF

--- backend/assembly/upload/urls.py
+++ backend/assembly/upload/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from . import views
 
-urlpatterns =[
-    path('upload/',views.upload_view),
+urlpatterns = [
+    path('upload', views.upload_view),
 ]
--- backend/assembly/upload/views.py
+++ backend/assembly/upload/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+
 # Create your views here.
 
 
+@require_POST
+@csrf_exempt
 def upload_view(request):
-
-    return HttpResponse("Hello World !")
+    manual_pdf = request.FILES.get('manual_pdf')
+    if manual_pdf and manual_pdf.content_type == 'application/pdf':
+        # Process the PDF file
+        return HttpResponse('{} file received'.format(manual_pdf.name))
+    return HttpResponse('Invalid file type', status=400)
